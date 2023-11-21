0f756bbe4658756067b1ba639d90348dff2daf27
makeit80 <hidlruddnr98@gmail.com>
2023-11-22 00:22:52 +0900

    google trends axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
+
+// https://cors-anywhere.herokuapp.com/corsdemo
 
 function Home() {
-
+  useEffect(() => {
+    const cheerio = require('cheerio');
+    const getRss = async () => {
+      try {
+        return await axios.get(`https://cors-anywhere.herokuapp.com/https://trends.google.co.kr/trends/trendingsearches/daily/rss?geo=KR`)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getRss()
+    .then(html => {
+      const $ = cheerio.load(html.data)
+      let keyword = [];
+      let date = [];
+      let traffic = [];
+      let title = [];
+      let source = []; 
+  
+      $('item').each((i, el) => {
+        keyword.push($(el).find('title').text())
+        date.push($(el).find('pubDate').text())
+        traffic.push($(el).children('ht\\:approx_traffic').text())
+      })
+      
+      $('ht\\:news_item').each((i, el) => {
+        title.push($(el).find('ht\\:news_item_title').text())
+        source.push($(el).find('ht\\:news_item_source').text())
+      })
+      console.log(keyword)
+  
+    })
+    .then(res => {
+      console.log(res)
+    });
+  }, [])
   
 
+
+
+
+
   return (
-    <div>Dev브랜치입니다!!</div>
+    <>
+      <div>Dev브랜치입니다!!</div>
+    </>
+
   )
 }
 
-export default Home
+export default Home;
