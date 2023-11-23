c541e2fa3fd394abe4cdf2064cef6d624a6b3bfa
mirae <mirae9873@gmail.com>
2023-11-23 14:56:54 +0900

    변경사항 없음

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,14 @@ import SignUpForm from './SignUpForm';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router';
 
-
 function Navbar() {
   const [user, setUser] = useState();
   const [openRegister, setOpenRegister] = useState(false);
   const [openLoginModal, setOpenLoginModal] = useState(false);
 
   const handleLogin = () => {
-    googleLogin()
-      .then((user) => setUser(user));
-  }
+    googleLogin().then((user) => setUser(user));
+  };
 
   const handleLogout = () => {
     logout().then((user) => setUser(user));
@@ -39,38 +37,41 @@ function Navbar() {
         <h1>Trend News</h1>
         <div>
           {user && <User user={user} />}
-          {user && (<button onClick={() => navigate(`/mypage/${user.uid}`)}>My Page</button>)}
-          {user ? (<button onClick={handleLogout}>logout</button>) : (<button onClick={() => setOpenLoginModal(true)}>login</button>)}
+          {user && <button onClick={() => navigate(`/mypage/${user.uid}`)}>My Page</button>}
+          {user ? (
+            <button onClick={handleLogout}>logout</button>
+          ) : (
+            <button onClick={() => setOpenLoginModal(true)}>login</button>
+          )}
           {!user && <button onClick={() => gotoSignUpPage()}>회원가입</button>}
         </div>
       </Nav>
       {/* 유저가 없고 회원가입 버튼을 눌렀을 때 회원가입 페이지에 회원가입 폼 띄움 */}
-      {!user && openRegister && <SignUpForm />}
-
+      {/* {!user && openRegister && <SignUpForm />} */}
     </>
   );
 }
 
 const Nav = styled.nav`
   height: 2.5rem;
- display:flex;
- justify-content:space-between;
- background-color: var(--color-bright-blue);
- color:white;
+  display: flex;
+  justify-content: space-between;
+  background-color: var(--color-bright-blue);
+  color: white;
 
- h1{
-  font-size:2rem;
- }
- 
-  div{
-    display:flex;
+  h1 {
+    font-size: 2rem;
   }
- button{
-  font-size:1.2rem;
-  color:white;
-  padding:0.5rem;
-  background-color: transparent;
- }
+
+  div {
+    display: flex;
+  }
+  button {
+    font-size: 1.2rem;
+    color: white;
+    padding: 0.5rem;
+    background-color: transparent;
+  }
 `;
 
-export default Navbar
+export default Navbar;
