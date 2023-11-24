cc0276871a8236659dd54dd27d964b8dcb6990be
mirae <mirae9873@gmail.com>
2023-11-24 14:19:44 +0900

    db에 comment 저장

--- src/redux/modules/comments.js
+++ src/redux/modules/comments.js
@@ -1,15 +1,25 @@
 const ADD_COMMENT = 'comments/ADD_COMMENT';
+const GET_COMMENT = 'comments/GET_COMMENT';
 
+// action creator
 export const addComment = (payload) => {
   return { type: ADD_COMMENT, payload };
+};
+export const getComment = (payload) => {
+  return { type: GET_COMMENT, payload };
 };
 
 const initialState = [];
 
+// reducer
 const comments = (state = initialState, action) => {
   switch (action.type) {
     case ADD_COMMENT:
+      console.log(action.payload);
       return [...state, action.payload];
+    case GET_COMMENT:
+      console.log(action.payload);
+      return [...state, ...action.payload];
     default:
       return state;
   }
